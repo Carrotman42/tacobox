11e3a14e62ed6de0c5a2408344444cf33fc10871
Carrotman42 <kevin@chowski.com>
2013-11-09 00:14:58 -0500

    Added consts file

---
+++ tacobox/src/com/karien/taco/mapstuff/C.java
@@ -0,0 +1,13 @@
+package com.karien.taco.mapstuff;
+
+/**
+ * A set of constants to talk to the map so that they are just in one place.
+ *
+ */
+public interface C {
+	String Blocked = "blocked";
+	String Visisble = "visible";
+	
+	boolean defaultBlocked = true;
+	boolean defaultVisible = true;
+}
