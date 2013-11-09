0804467a159a2960eac6acce0da36a3fdb32490f
Carrotman42 <kevin@chowski.com>
2013-11-09 14:04:54 -0500

    Started multiplayer

--- tacobox/src/com/karien/tacobox/MyTacoBox.java
+++ tacobox/src/com/karien/tacobox/MyTacoBox.java
@@ -1,9 +1,12 @@
 package com.karien.tacobox;
 
+import java.util.Scanner;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.karien.taco.mapstuff.LevelHelper;
+import com.karien.tacobox.comm.MultiplayerComm;
 import com.karien.tacobox.screens.LoadingScreen;
 import com.karien.tacobox.screens.MainScreen;
 
@@ -11,7 +14,7 @@ public class MyTacoBox extends Game {
 	private LevelHelper lvls;
 	private GameState state = GameState.Title;
 	
-	
+	private MultiplayerComm multi;
 	
 	@Override
 	public void create() {	
@@ -23,13 +26,14 @@ public class MyTacoBox extends Game {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		switch (state) {
 		case Title:
-			state = GameState.LoadFirstLevel;
+			state = GameState.ConnectMultiplayer;
 			break;
 		case ConnectMultiplayer:
-			
+			multi = connectMultiplayer();
+			state = GameState.LoadFirstLevel;
 			break;
 		case LoadFirstLevel:
-			lvls = new LevelHelper(null, this);
+			lvls = new LevelHelper(multi, this);
 			lvls.loadNextLevel();
 			setScreen(new LoadingScreen());
 			state = GameState.WaitLoadFirstLevel;
@@ -87,4 +91,30 @@ public class MyTacoBox extends Game {
 			state = GameState.LoadFirstLevel;
 		}
 	}
+	
+	private int port = 4608;
+	private MultiplayerComm connectMultiplayer() {
+		System.out.println("Connecting multiplayer");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			if (sc.nextLine().equals("")) {
+				return MultiplayerComm.connect(port);
+			} else {
+				return MultiplayerComm.connect("172.26.3.181", port);
+			}
+		}catch(Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
