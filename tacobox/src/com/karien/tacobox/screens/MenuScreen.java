9e6008a18c44d47d91c600f203e7d1d5afa5018b
Ben Smith <bls220@vt.edu>
2013-11-11 16:50:51 -0500

    Moved skin to json and support files. Starting creating a dialog for ip

--- tacobox/src/com/karien/tacobox/screens/MenuScreen.java
+++ tacobox/src/com/karien/tacobox/screens/MenuScreen.java
@@ -1,18 +1,18 @@
 package com.karien.tacobox.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.karien.tacobox.MyTacoBox;
@@ -36,23 +36,21 @@ public class MenuScreen implements Screen {
 		// fine. Using a skin is optional but strongly
 		// recommended solely for the convenience of getting a texture, region,
 		// etc as a drawable, tinted drawable, etc.
-		skin = new Skin();
+		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 
-		// Store the default libgdx font under the name "default".
-		BitmapFont font = new BitmapFont();
-		font.setScale(2f);
-		skin.add("default", font);
+		// Create Dialog
+		Label ipLabel = new Label("Host Address: ", skin);
+		final TextField ipTextField = new TextField("127.0.0.1", skin);
 
-		// Configure a LabelStyle and name it default
-		LabelStyle labelStyle = new LabelStyle();
-		labelStyle.font = skin.getFont("default");
-		labelStyle.fontColor = Color.YELLOW;
-		skin.add("default", labelStyle);
-
-		TextButtonStyle buttonStyle = new TextButtonStyle();
-		buttonStyle.font = skin.getFont("default");
-		buttonStyle.fontColor = Color.YELLOW;
-		skin.add("default", buttonStyle);
+		Window window = new Window("Dialog", skin);
+		window.getButtonTable().add(new TextButton("X", skin))
+				.height(window.getPadTop());
+		window.setPosition(0, 0);
+		window.defaults().spaceBottom(10);
+		window.row().fill().expandX();
+		window.add(ipLabel);
+		window.add(ipTextField);
+		window.pack();
 
 		// Create a table that fills the screen. Everything else will go inside
 		// this table.
@@ -69,6 +67,7 @@ public class MenuScreen implements Screen {
 
 		// create buttons
 		TextButton quitBtn = new TextButton("Quit", skin);
+		quitBtn.getLabel().setFontScale(2f);
 		quitBtn.addListener(new ClickListener() {
 			public void clicked(InputEvent event, float x, float y) {
 				throw new RuntimeException("Quit");
@@ -76,6 +75,7 @@ public class MenuScreen implements Screen {
 		});
 
 		TextButton hostBtn = new TextButton("Host", skin);
+		hostBtn.getLabel().setFontScale(2f);
 		hostBtn.addListener(new ClickListener() {
 			public void clicked(InputEvent event, float x, float y) {
 				parent.menuChoice("host");
@@ -83,20 +83,27 @@ public class MenuScreen implements Screen {
 		});
 
 		TextButton joinBtn = new TextButton("Join", skin);
+		joinBtn.getLabel().setFontScale(2f);
 		joinBtn.addListener(new ClickListener() {
 			public void clicked(InputEvent event, float x, float y) {
-				parent.menuChoice("join");
+				new Dialog("Connect to...", skin, "dialog") {
+					protected void result(Object object) {
+						System.out.println("Chosen: " + ipTextField.getText());
+						parent.menuChoice("join");
+					}
+				}.button("Connect", true).button("Cancel", false)
+						.key(Keys.ENTER, true).key(Keys.ESCAPE, false)
+						.show(stage);
 			}
 		});
 
 		container.add(titleLabel).align(Align.center).expandX();
 		container.row();
-		container.add(hostBtn).align(Align.center | Align.bottom).expand()
-				.pad(10);
+		container.add(hostBtn).align(Align.center | Align.bottom).expand();
 		container.row();
-		container.add(joinBtn).align(Align.center).expandX().pad(10);
+		container.add(joinBtn).align(Align.center).expandX();
 		container.row();
-		container.add(quitBtn).align(Align.center | Align.top).expand().pad(10);
+		container.add(quitBtn).align(Align.center | Align.top).expand();
 	}
 
 	@Override
