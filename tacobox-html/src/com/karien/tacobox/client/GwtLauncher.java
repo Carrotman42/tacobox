a77d6ad4f0feb5895897c555f0d2d085749a7207
Ben Smith <bls220@vt.edu>
2013-11-13 17:40:05 -0500

    Added fixed view size to html version.

--- tacobox-html/src/com/karien/tacobox/client/GwtLauncher.java
+++ tacobox-html/src/com/karien/tacobox/client/GwtLauncher.java
@@ -1,19 +1,20 @@
 package com.karien.tacobox.client;
 
-import com.karien.tacobox.MyTacoBox;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.karien.tacobox.MyTacoBox;
 
 public class GwtLauncher extends GwtApplication {
 	@Override
-	public GwtApplicationConfiguration getConfig () {
-		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+	public GwtApplicationConfiguration getConfig() {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(
+				MyTacoBox.SCREEN_WIDTH, MyTacoBox.SCREEN_HEIGHT);
 		return cfg;
 	}
 
 	@Override
-	public ApplicationListener getApplicationListener () {
+	public ApplicationListener getApplicationListener() {
 		return new MyTacoBox();
 	}
 }
